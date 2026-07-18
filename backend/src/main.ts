3a382833131b66346b16b38ab3f8c7b18c52273f
Nuttapa <yngsilpwiriyakul@gmail.com>
2026-07-18 22:40:54 +0700

    fix render port

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,11 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
 
-  await app.listen(3001);
+  const port = process.env.PORT || 3001;
+
+
+  await app.listen(port);
+
 }
 
 bootstrap();
