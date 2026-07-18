545a0c7b9da9bfe3f9395de8892acc793cd3bcd3
Nuttapa <yngsilpwiriyakul@gmail.com>
2026-07-18 23:20:33 +0700

    check mongo url

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -8,57 +8,38 @@ import { IngredientModule } from './ingredient/ingredient.module';
 import { MenuModule } from './menu/menu.module';
 
 
-
-
 @Module({
-
   imports: [
 
-
     ConfigModule.forRoot({
-
       isGlobal: true,
-
     }),
-
-
-
 
     MongooseModule.forRootAsync({
 
       imports: [ConfigModule],
 
+      useFactory: () => {
 
-      useFactory: () => ({
+        console.log("MONGO URL:", process.env.MONGODB_URI);
 
-        uri: process.env.MONGODB_URI,
+        return {
+          uri: process.env.MONGODB_URI,
+        };
 
-      }),
+      },
 
-
-    }),
-
-
-
+    }), // <-- ตัวนี้หายไป
 
     IngredientModule,
 
-
     MenuModule,
 
-
-
   ],
-
-
-
 
   controllers: [
-
     AppController
-
   ],
-
 
 })
 
